23b20fad0c8dc370225f851e85ba1f2a29b1317f
@bishal_sapkota <45955052+bishalsapkota0@users.noreply.github.com>
2026-05-15 15:35:23 +0000

    basic operations

--- basic_01/03_conversionOperation.js
+++ basic_01/03_conversionOperation.js
@@ -21,3 +21,36 @@ let someNumber = 33;
 let stringNUmber = String(someNumber);
 console.log(stringNUmber);
 console.log(typeof(stringNUmber));
+
+///****************Operations *********************** */
+
+let value = 3;
+let negValue = -value;
+console.log(value+negValue);
+//** power raised */
+//% remainder
+
+let str1 = "bishal";
+let str2 = " hello";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log(+true);
+
+let x = 3;
+const y= x++;
+console.log(x);
+console.log(y);
+//??
+//y = 3 then x = 4
+
+
+let a = 3;
+const b = ++a;
+console.log(a);
+console.log(b);
+//a = 4 and then b = 4
