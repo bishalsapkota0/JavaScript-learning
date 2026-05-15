5495c34d7996d8a5cf4f7678b055a6dcd73dd173
@bishal_sapkota <45955052+bishalsapkota0@users.noreply.github.com>
2026-05-15 15:20:29 +0000

    parse

---
+++ basic_01/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+// "33"-->33
+//33abc -->NaN 
+//true -->1
+//false -->0
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//""-->false
+//"bishal"-->true
+
+let someNumber = 33;
+let stringNUmber = String(someNumber);
+console.log(stringNUmber);
+console.log(typeof(stringNUmber));
