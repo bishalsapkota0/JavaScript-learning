d2bd42942995b19ec539ca6af09e1d568942bd9b
@bishal_sapkota <45955052+bishalsapkota0@users.noreply.github.com>
2026-05-15 15:20:39 +0000

    parse

---
+++ basic_01/01_variables.js
@@ -0,0 +1,12 @@
+const accountId  = 144553
+let accountEmail = "bishal@gmail.com"
+var accountPassword = "123454"
+accountCity  ="Jaipur"
+accountEmail = "bibi@gmail.com"
+//accountId = 2
+console.log(accountId);
+let accountState
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+//do not use var
+// declare veriable before using that variable 
+
---
+++ basic_01/02_varibales.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all code as newer version 
+
+//alert(3+3) we are using node.js not browser -> here we word differentlly
+
+
+console.log(3+3)
+console.log("bishal")
+//data types
+//null - . represents null value or empty value 
+//string 
+//boolean
+//string
+//symbol - used for uniqueness
+//object 
+console.log(typeof "bishal");
+console.log(typeof undefined);
+console.log(typeof null);
